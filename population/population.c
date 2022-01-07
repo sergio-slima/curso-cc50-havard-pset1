91f773323b5f8516a1df80ecdb634d620e61b83d
sergio-slima <sergio_silvalima@hotmail.com>
2022-01-07 18:48:31 +0000

    Desafio population

---
+++ population/population.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+      // TODO: Solicite o valor inicial ao usuário
+        int x;
+        do
+        {
+            x = get_int("População inicial: ");
+        }
+        while (x < 9);
+
+     // TODO: Solicite o valor final ao usuário
+        int y;
+        do
+        {
+            y = get_int("População final: ");
+        }
+        while (y <= x);
+
+     // TODO: Calcule o número de anos até o limite
+        int n = 0;
+        int z = x;
+        while (z < y)
+        {
+            z = z + ((z / 3) - (z / 4));
+            n++;
+        }
+
+     // TODO: Imprima o número de anos
+        printf("Years: %i\n", n);
+
+}
